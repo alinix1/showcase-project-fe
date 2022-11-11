915c275e76f7bb26e5cd9e7dd6a0947262148a97
Ali <anix1@kent.edu>
2022-11-11 00:00:02 -0700

    fix node_modules and add Sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+    <div className="sidebar-container">
+        <button>H O M E</button>
+    </div>
+}
+
+export default Sidebar
