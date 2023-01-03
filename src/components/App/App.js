fa41e9cbec65cbdb998055d81c543facfbe1bebc
Ali <anix1@kent.edu>
2023-01-03 16:47:51 -0700

    refactor using hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Songs from '../Songs/Songs'
 import getSongsData from '../../apiCalls'
@@ -6,50 +6,38 @@ import Header from '../Header/Header'
 import Playlist from '../Playlist/Playlist'
 import'./App.css'
 
-
-
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      songs: [],
-      favoriteSongs: [],
-      errorMessage: ""
-    }
-  }
-
-  componentDidMount() {
+const App = () => {
+  const [songs, setSongs] = useState([])
+  const [favoriteSongs, setFavoriteSongs] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+ 
+  useEffect(() => { 
     getSongsData(`/songs`)
-    .then((data) => this.setState({ songs: data.songs }))
+    .then((data) => setSongs(data.songs))
     .catch((error) => {
-      this.setState({
-        ...this.state,
-        errorMessage: 'An error occurred, please try again.'
-      })
+      setErrorMessage( 'An error occurred, please try again.')
     })
-  }
+  },[])
 
-  saveSongPlaylist = (id) => {
-    const favoriteSong = this.state.songs.find(song => song.id === id)
-    if (!this.state.favoriteSongs.includes(favoriteSong)) {
-      this.setState({ favoriteSongs: [...this.state.favoriteSongs, favoriteSong] })
+  const saveSongPlaylist = (id) => {
+    const favoriteSong = songs.find(song => song.id === id)
+    if (!favoriteSongs.includes(favoriteSong)) {
+      setFavoriteSongs(...favoriteSongs, favoriteSong)
     }
   }
 
-  deleteSong = (id) => {
-    const deleteFavoriteSong = this.state.favoriteSongs.filter(song => song.id !== id)
-    this.setState({ favoriteSongs: deleteFavoriteSong})
+  const deleteSong = (id) => {
+    const deleteFavoriteSong = favoriteSongs.filter(song => song.id !== id)
+    setFavoriteSongs(deleteFavoriteSong)
    
   }
-  
-  render() {
     return (
     <div className='app-container'>
       <main>
       <Header />
       <Switch>
-      <Route exact path='/' render={() => <Songs songs={this.state.songs} favoriteSongs={this.state.favoriteSongs} saveSongPlaylist={this.saveSongPlaylist} deleteSong={this.deleteSong}  />} />
-      <Route exact path='/playlist' render={() => <Playlist favoriteSongs={this.state.favoriteSongs} saveSongPlaylist={this.saveSongPlaylist} deleteSong={this.deleteSong} />}/>
+      <Route exact path='/' render={() => <Songs songs={songs} favoriteSongs={favoriteSongs} saveSongPlaylist={saveSongPlaylist} deleteSong={deleteSong}  />} />
+      <Route exact path='/playlist' render={() => <Playlist favoriteSongs={favoriteSongs} saveSongPlaylist={saveSongPlaylist} deleteSong={deleteSong} />}/>
       <Route path='*'>
             <h3>404: Sorry, that page doesn't exist.</h3>
       </Route>
@@ -58,7 +46,5 @@ class App extends Component {
     </div> 
     )
   }
-}
-
 
 export default App;
