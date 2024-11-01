84b61190d931864c920285233563bd52db6ba62c
Ali <anix1@kent.edu>
2024-10-31 20:57:08 -0600

    tweak header layout for responsive design and continue updating styles for the card component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,21 +12,23 @@ const Header = () => {
         src={headphones}
         alt="logo of headphones"
       />
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <button className="home-button-container" data-cy="home-btn">
-          H O M E
-        </button>
-      </Link>
-      <Link to="/playlist" style={{ textDecoration: "none" }}>
-        <button className="playlist-button-container" data-cy="playlist-btn">
-          P L A Y L I S T
-        </button>
-      </Link>
-      <Link to="/about" style={{ textDecoration: "none" }}>
-        <button className="about-button-container" data-cy="about-btn">
-          A B O U T
-        </button>
-      </Link>
+      <div className="all-button-container">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <button className="home-button-container" data-cy="home-btn">
+            H O M E
+          </button>
+        </Link>
+        <Link to="/playlist" style={{ textDecoration: "none" }}>
+          <button className="playlist-button-container" data-cy="playlist-btn">
+            P L A Y L I S T
+          </button>
+        </Link>
+        <Link to="/about" style={{ textDecoration: "none" }}>
+          <button className="about-button-container" data-cy="about-btn">
+            A B O U T
+          </button>
+        </Link>
+      </div>
     </header>
   );
 };
