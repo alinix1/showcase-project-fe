cf2706492aa978ce314f2e6c8371f922747875e3
alinix1 <anix1@kent.edu>
2025-01-29 16:42:13 -0700

    remove comments in code for testing

--- cypress/e2e/loading_state_spec.cy.js
+++ cypress/e2e/loading_state_spec.cy.js
@@ -1,12 +1,11 @@
 describe("Playlist Page - Loading State", () => {
   beforeEach(() => {
-    // Intercept the API request for songs
     cy.intercept(
       "GET",
       "https://beats4devs-api.herokuapp.com/api/v1/songs",
       (req) => {
         req.reply({
-          fixture: "songs", // Use the fixture for songs
+          fixture: "songs",
           statusCode: 200,
           ok: true,
         });
@@ -17,18 +16,14 @@ describe("Playlist Page - Loading State", () => {
   });
 
   it("Should render a loading icon and message while content is loading", () => {
-    // Check that the loading text and icon are visible initially
     cy.get('[data-cy="loading-text-element"]')
       .should("be.visible")
       .contains("...loading your music");
 
-    // Wait for a moment before sending the API response (simulate loading state)
-    cy.wait(300); // You can adjust this delay as needed for your test scenario
+    cy.wait(300);
 
-    // Wait for the intercepted request to finish
     cy.wait("@getSongsWithLoading");
 
-    // After loading, make sure the loading text disappears and the content is shown
     cy.get('[data-cy="loading-text-element"]').should("not.exist");
     cy.get('[data-cy="songs-container-element"]').should("exist");
   });
